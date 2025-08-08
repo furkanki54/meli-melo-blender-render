bc25f0c11fb296f2dee325f3e3d809559b8f9cc6
furkanki54 <132562462+furkanki54@users.noreply.github.com>
2025-08-08 13:08:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import os
 import sys
 
-# scenes klasörünü oluştur
 os.makedirs("scenes", exist_ok=True)
 
-# Headless (GUI’siz) Blender çalıştırma
-exit_code = os.system("blender --background --python scene.py")
+# Xvfb ile headless Blender
+cmd = "xvfb-run -a blender -b --python scene.py"
+print("→ Çalıştırılıyor:", cmd)
+exit_code = os.system(cmd)
+
 if exit_code != 0:
     print("❌ Blender çalıştırılamadı. Loglara bak.")
     sys.exit(1)
